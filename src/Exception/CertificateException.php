8e9449082d98b31ccef8c94b25bd2948e067e705
Manito <eumanito@gmail.com>
2020-09-04 15:48:15 -0300

    Tradução das mensagens
    Sugiro a tradução das mensagem de erro para o idioma Português BR, por tratar-se de um biblioteca para nota fiscal brasileira.

--- src/Exception/CertificateException.php
+++ src/Exception/CertificateException.php
@@ -14,36 +14,36 @@ class CertificateException extends \RuntimeException implements ExceptionInterfa
 {
     public static function unableToRead()
     {
-        return new static('Unable to read certificate, ' . static::getOpenSSLError());
+        return new static('Não foi possível ler o certificado digital, ' . static::getOpenSSLError());
     }
 
     public static function unableToOpen()
     {
-        return new static('Unable to open certificate, ' . static::getOpenSSLError());
+        return new static('Não foi possível abrir o certificado digital, ' . static::getOpenSSLError());
     }
 
     public static function signContent()
     {
         return new static(
-            'An unexpected error has occurred when sign a content, ' . static::getOpenSSLError()
+            'Ocorreu um erro ao assinar o conteúdo com certificado digital, ' . static::getOpenSSLError()
         );
     }
 
     public static function getPrivateKey()
     {
-        return new static('An error has occurred when get private key, ' . static::getOpenSSLError());
+        return new static('Ocorreu um erro ao ler chave privada do certificado digital, ' . static::getOpenSSLError());
     }
 
     public static function signatureFailed()
     {
         return new static(
-            'An error has occurred when verify signature, ' . static::getOpenSSLError()
+            'Ocorreu um erro ao verificar assinatura do certificado digital, ' . static::getOpenSSLError()
         );
     }
     
     protected static function getOpenSSLError()
     {
-        $error = 'get follow error: ';
+        $error = 'verifique os erros: ';
         while ($msg = openssl_error_string()) {
             $error .= "($msg)";
         }
